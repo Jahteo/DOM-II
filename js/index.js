b42e3199b3084878a83631ab9daf84e3c983d898
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-04 20:47:04 -0700

    tried all sorts of problem solving for running a forEach with the nav-link's. Incl working w taja & the whole TL group. got code extra wonky, going to comment it all out & start simpler tasks from scratch, but the giant commented part is a good study for some intricacy we were all missing.

--- js/index.js
+++ js/index.js
@@ -8,23 +8,30 @@ imgs.forEach(element => element.addEventListener("mouseover", (event) => event.t
 // * `keydown` all navLinks ->green
 const navLinks = Array.from(document.getElementsByClassName("nav-link"));
 
+//const navLinks1 = //pretty sure this is the solution, break everything into simpler pieces.
+
 // // //This attempt to do it from a different angle sucks. way too many problems. I eventually want to know why, but right now, I just want my sanity.
 // document.addEventListener("keydown", (event) => {
-//     debugger
+//     debugger //for some reason, this is also not running properly. It keeps stopping on the final line of JS, instead of this line & each subsequent line. Save, close and restart everything??
 //     if (event.key === "Escape") { //magical issue number 1: only 'Escape" works. Nothing else. because yeah.
-//         return navLinks.forEach(event => event.target.style.color = "green") //magical issue number2: this requires a return to escape the if statement. number3: somehow, event.target.style.color doesn't actually grab anything, even though it works exactly the same on line 4.
+//         return navLinks.forEach(event =>
+//             // console.log(event));
+//             //  //shows that links inside forEach are beign changed to html style, regardless of whether originally pulled with getElementsby or querySelector. Test line 4 to see if that is normal behavior or the problem.
+//             event.setAttribute('color', 'green'))
+//         //  navLinks.style.color = "green") //magical issue number2: this requires a return to escape the if statement. number3: somehow, event.target.style.color doesn't actually grab anything, even though it works exactly the same on line 4. we tried event, event.target, navLinks, & a few other variations.
 //     }
 // })
+// console.log(navLinks)
 
-// //   and this one just doesn't trigger for some reason... except for when it randomly does...
+//   and this one just doesn't trigger for some reason... except for when it randomly does...
 // navLinks.forEach(element => element.addEventListener("keydown", (event) => {
 //     debugger
 //     if (event.key === "Escape") {
-//         event.target.style.color = "green"
+//         return event.target.style.color = "green"
 //     }
 // }))
 
-
+//  //  copied straight from lecture code for reference.
 // 'keydown', event => {
 //     // if the key pressed was ESC, dismiss the modal, otherwise nothing
 //     if (event.key === 'Escape') {
